f693b35a721351e064d33fe9b456431d525af56f
tveiz <tm9034156@gmail.com>
2026-01-08 11:58:01 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['formidable'],
+  },
+};
 
+export default nextConfig;
